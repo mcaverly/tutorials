6560c0c2e4a4d11a67a4cd68ac7cd91a253f745d
Michael Caverly <m.caverly30@gmail.com>
2019-11-18 20:46:58 -0500

    Refactoring demo app

---
+++ spring-boot-angular/demo/src/main/java/com/baeldung/demo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.baeldung.demo.repositories;
+
+import com.baeldung.demo.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{}
